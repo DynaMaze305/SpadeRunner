52ca5b051e4250232b8c31b90cb5dc1f4f9bcea0
szpy <savioz.py@protonmail.com>
2026-04-16 18:21:28 +0200

    doc: adding comments to the whole codebase

--- src/photo_io.py
+++ src/photo_io.py
@@ -5,7 +5,7 @@ import aiofiles
 
 PHOTOS_DIR = os.path.join(os.getcwd(), "received_photos")
 
-
+# Wrapper that saves the picture in the directory
 async def save_bytes(data: bytes, name: str, directory: str = PHOTOS_DIR) -> str:
     os.makedirs(directory, exist_ok=True)
     path = os.path.join(directory, name)
