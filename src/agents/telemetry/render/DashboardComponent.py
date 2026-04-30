78724f7f344581cf17f3853b13dfe8aefe445a52
mariethoz <54270350+mariethoz@users.noreply.github.com>
2026-04-30 10:21:07 +0200

    feat: split dashboard component in class

---
+++ src/agents/telemetry/render/DashboardComponent.py
@@ -0,0 +1,13 @@
+class DashboardComponent:
+    def render_html(self):
+        return ""
+
+    def render_css(self):
+        return ""
+
+    def render_js(self):
+        return ""
+
+    def update_js(self):
+        """JS function called when telemetry arrives"""
+        return ""
